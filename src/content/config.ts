c57cbbf626aa35d576daee21dbb8c47b99d8f9d9
Felipe Navas <felipenavas.itec@gmail.com>
2025-07-08 20:45:30 -0300

    feat: Update blog post images and enhance image handling in components

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,16 +2,17 @@ import { defineCollection, z } from 'astro:content'
 
 const blogCollection = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string(),
-    image: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: image(),
 
-    // Relaciones
-    author: z.string(),
-    tags: z.array(z.string()),
-  }),
+      // Relaciones
+      author: z.string(),
+      tags: z.array(z.string()),
+    }),
 })
 
 export const collections = {
